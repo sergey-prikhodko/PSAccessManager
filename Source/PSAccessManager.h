eb104e7666bc975b62167279235bc6e11f19747f
sergey-prrikhodko <sergey.prikhodko.95@gmail.com>
2020-02-12 14:35:14 +0200

    setup project

---
+++ Source/PSAccessManager.h
@@ -0,0 +1,19 @@
+//
+//  PSAccessManager.h
+//  PSAccessManager
+//
+//  Created by Sergey Prikhodko on 12.02.2020.
+//  Copyright © 2020 Sergey Prikhodko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PSAccessManager.
+FOUNDATION_EXPORT double PSAccessManagerVersionNumber;
+
+//! Project version string for PSAccessManager.
+FOUNDATION_EXPORT const unsigned char PSAccessManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PSAccessManager/PublicHeader.h>
+
+
