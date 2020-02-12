9d3b6f25702ee1ef762969eb583601d034d0deda
sergey-prrikhodko <sergey.prikhodko.95@gmail.com>
2020-02-12 14:55:17 +0200

    setup magic func

---
+++ Source/Magic.swift
@@ -0,0 +1,16 @@
+//
+//  Magic.swift
+//  PSAccessManager
+//
+//  Created by Sergey Prikhodko on 12.02.2020.
+//  Copyright © 2020 Sergey Prikhodko. All rights reserved.
+//
+
+import Foundation
+
+public final class MagicClass {
+    
+    static func doSomeMagic() -> String {
+        return "This is a magic!"
+    }
+}
